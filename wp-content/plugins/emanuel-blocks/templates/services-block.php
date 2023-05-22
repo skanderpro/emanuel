689b4f27d6d9adf7c244dafabbed2f2d3e05412c
Bryniuk Valia <valyanychyporuk@gmail.com>
2023-05-23 00:07:11 +0300

    - uat fioxes

--- wp-content/plugins/emanuel-blocks/templates/services-block.php
+++ wp-content/plugins/emanuel-blocks/templates/services-block.php
@@ -7,26 +7,22 @@
 				</p>
 			</div>
 			<?php
-			$query = new WP_Query(array(
-				'post_type' => 'services',
-				'post_status' => 'publish'
-			));
-			while ($query->have_posts()) {
-				$query->the_post();
+
+			foreach ( $settings['items'] as $item ) {
 				?>
                 <div class="services-item">
                     <div class="services-item-head">
                         <div class="services-item-head-img">
                             <picture>
-                                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" /></picture>
+                                <img src="<?php echo $item['image']['url'] ?? ''; ?>" alt="<?php echo $item['title']; ?>" /></picture>
                         </div>
                     </div>
                     <div class="services-item__inner">
                         <div class="services-item__inner-text">
-                            <h4><?php the_title(); ?></h4>
+                            <h4><?php echo $item['title']; ?></h4>
                         </div>
 
-                        <a href="<?php the_permalink(); ?>" class="global-outline-btn">Learn more</a>
+                        <a href="<?php echo $item['link']; ?>" class="global-outline-btn"><?php echo $item['btn_text']; ?></a>
                     </div>
                 </div>
 				<?php
