9bcc402f966ccae314b7f61cbe9c7e3e57990f88
kiptykDima <kiptykdmytro@gmail.com>
2025-11-14 14:14:13 +0200

    added interface

--- wp-content/themes/emanuel/vendor/composer/autoload_real.php
+++ wp-content/themes/emanuel/vendor/composer/autoload_real.php
@@ -23,30 +23,11 @@ class ComposerAutoloaderInit739c40e96950053f1cd2d774f0c611d2
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit739c40e96950053f1cd2d774f0c611d2', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit739c40e96950053f1cd2d774f0c611d2', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit739c40e96950053f1cd2d774f0c611d2::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit739c40e96950053f1cd2d774f0c611d2::getInitializer($loader));
 
         $loader->register(true);
 
